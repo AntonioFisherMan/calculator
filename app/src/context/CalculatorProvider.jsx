1ac2738e5a3a4170c77ef226a644b0e4bb12b6b9
Антон Кийко <antonkijko@iMac-Anton.local>
2021-02-15 21:27:54 +0200

    fix hadnlesetnumber function

--- app/src/context/CalculatorProvider.jsx
+++ app/src/context/CalculatorProvider.jsx
@@ -10,44 +10,26 @@ const CalculatorProvider = (props) => {
 
         const handleSetNumber = value => {
                 setResult("")
-                if (value === "1" && result !== ""
-                        || value === "2" && result !== ""
-                        || value === "3" && result !== ""
-                        || value === "4" && result !== ""
-                        || value === "5" && result !== ""
-                        || value === "6" && result !== ""
-                        || value === "7" && result !== ""
-                        || value === "8" && result !== ""
-                        || value === "9" && result !== ""
-                        || value === "0" && result !== "") {
+                if (Number(value) <= 9 && result !== "") {
                         setExpression(value)
-
                 }
                 else {
-
                         setExpression(prev => prev + value)
                 }
 
         }
         const handleResult = () => {
-
                 try {
                         setResult(math.evaluate(expression));
                         setExpression(math.evaluate(expression).toString());
                 } catch (error) {
                         alert("Input correct expression")
                 }
-
-
-
-
-
         }
         const handleBack = () => {
 
                 setExpression(prev => prev.length <= 1 ? "" : prev.slice(0, -1))
-        };
-
+        }
         const handleClear = () => {
                 setExpression("")
                 setResult("")
